9563bd270adf478f342479b0cf3d4d60a2d24c7e
Yen Nguyen <nhpyen315@gmail.com>
2022-01-30 20:09:15 +0700

    Add thuchanh1.js

---
+++ thuchanh1.js
@@ -0,0 +1,21 @@
+let nguoinhan;
+let diadiem;
+let thoigian;
+
+nguoinhan=prompt('Nhập tên người nhận:');
+diadiem=prompt('Nhập địa điểm:');
+thoigian=prompt('Nhập thời gian:');
+
+document.write("Joe thương nhớ,<br> <br>");
+document.write("Em không biết phải nói sao để anh hiểu rằng, em nhớ anh thật nhiều. " 
++ "Em yêu anh đến khi trái tim này tan thành nghìn mảnh. "
++ "<br> Tất cả những gì em yêu thương, em khát khao và cần đến, chính là anh, mãi mãi về sau. "
++ "Em chỉ muốn ở bên anh, và anh yêu hỡi, <br> em sẽ trở thành người phụ nữ như anh mong muốn. <br> <br>");
+document.write("Có phải em thật tệ hại, khi cứ nghĩ đến anh thật nhiều, thật lâu và nhất là mỗi khi đêm xuống?"+ 
+"Em hứa sẽ sẽ cố gắng triệu triệu <br>  lần hơn thế. Nhưng hơn tất cả, em chỉ mong một ngày nào đó, anh sẽ tự hào về em,"+
+ "như tự hào về vợ của anh, và mẹ của các<br> con anh (ít nhất là 2 nhé, em vừa mới quyết định đấy!)."+
+  "Em nhớ thật nhiều cảm giác mỗi đêm anh ôm em và ru em ngủ trong<br> vòng tay." +
+  "Đêm nay, em chỉ muốn được gần bên anh… và anh biết không, trái tim em đang đau đớn đến nhường nào. <br> <br>");
+
+  document.write("Anh yêu thương, đừng bao giờ rời xa em nữa nhé. Yêu anh rất nhiều. <br> <br>");
+  document.write("Los Angeles, ngày 10 tháng 10 năm 1951.");
